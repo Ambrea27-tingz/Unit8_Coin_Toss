32538b6c0b66e582a59c2dab72d8c1eb4662da05
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-06-22 17:10:28 -0400

    Created a class called coin to define the steps for the game an its stages.

--- Lab8_awilliams#1.py
+++ Lab8_awilliams#1.py
@@ -9,3 +9,22 @@
    """
 
 import random
+
+class Coin:
+    def __init__(self):
+        self.__amount = 20
+        self.__sideup = random.choice(["Heads", "Tails"])
+
+    def toss(self):
+        self.__sideup = "Heads" if random.randint(0, 1) == 0 else "Tails"
+
+    def get_sideup(self):
+        return self.__sideup
+
+    def set_amount(self, change):
+        self.__amount += change
+
+    def get_amount(self):
+        return self.__amount
+    
+    

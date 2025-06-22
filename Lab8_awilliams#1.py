8129159898559d0dd89d8a0989775edaae2d63ad
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-06-22 16:38:39 -0400

    Introduced the code and used the import random funnction to impliment the use for a range in a set of numbers for the game.

---
+++ Lab8_awilliams#1.py
@@ -0,0 +1,11 @@
+"""Name: Ambrea Williams
+   
+   Date: 06/21/2025
+
+   Unit 8: Lab 8 - Coin Toss Game
+
+   Description: Coin toss is a game where the user guesses heads or tails.
+   The program simulates a coin toss and checks if the user's guess is correct.
+   """
+
+import random
